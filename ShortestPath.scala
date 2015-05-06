93165218a09aa5673cbeeb9db39bee20749df11b
Benoît <benoitdancoisne@gmail.com>
2015-05-06 15:13:28 -0700

    S.Path in terms of # of edges + keep track of minimum capacity along the S.Path

--- ShortestPath.scala
+++ ShortestPath.scala
@@ -32,36 +32,46 @@ val graph = Graph(vertices, edges)
 val sourceId: VertexId = 1 // The source
 val targetId: VertexId = 4 // The target
 val test: VertexId = 5 // default vertex id, probably need to change
+
+
 // Initialize the graph such that all vertices except the root have distance infinity.
-// Note that now vertices will be of type [VertexId, (distance,id)]
-val initialGraph = graph.mapVertices( (id, _) => if (id == sourceId) (0.0, id) else (Double.PositiveInfinity, id))
+// Note that now vertices will be of type [VertexId, (distance,mincap,id)]
+
+val initialGraph = graph.mapVertices( (id, _) => if (id == sourceId) (0.0, Double.PositiveInfinity, id)
+																								 else (Double.PositiveInfinity, Double.PositiveInfinity, id))
 
 
-val sssp = initialGraph.pregel((Double.PositiveInfinity, test))(
-  (id, dist, newDist) => {
-    if (dist._1 < newDist._1) dist // dist(0) = first entry of (distance,id)
+val sssp = initialGraph.pregel((Double.PositiveInfinity, Double.PositiveInfinity, test))(
+  
+	// Vprog
+	(id, dist, newDist) => {
+    if (dist._1 < newDist._1) dist
     else newDist
   },
-  triplet => { // srcAttr, attribute of where message comes from
-    // dstAttr, attribute of destination
-    // attr of edge
+
+	// sendMsg
+  triplet => {
     // So if distance of source + weight of edge connecting to destination is
     // less than attribute of destination => update
-    if (triplet.srcAttr._1 + triplet.attr < triplet.dstAttr._1) {
-      Iterator((triplet.dstId, (triplet.srcAttr._1 + triplet.attr, triplet.srcId)))
-      // Now you change the new vertex to be [id of destination, (distance, sourceId)
+    if (triplet.srcAttr._1 + 1 < triplet.dstAttr._1) {
+      Iterator((triplet.dstId, (triplet.srcAttr._1 + 1, math.min(triplet.srcAttr._2,triplet.attr),triplet.srcId)))
     }
     else {
       Iterator.empty
     }
   },
+
+	// mergeMsg
   (a, b) => {
     if (a._1 < b._1) a
-    else b
+    else if ((a._1 == b._1)&&(a._2 > b._2)) a
+		else b
   }
 )
 
+sssp.vertices.collect()
 
+/* TODO : recover shortest path + min capaacity
 // What happens here if nodes are unvisited? We do not want those in our set. Add if != INF statement?
 val vNum = sssp.vertices.count.toInt // Number of elements < n
 val v = sssp.vertices.take(vNum) // Convert RDD to array
@@ -90,22 +100,20 @@ for ( i <- 0 to vNum-1 ) {
 
 println("Shortest Path is: ")
 println(path)
+*/
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-/*
-// Naive Implementation
-// Use ListBuffer as appending to List is O(n) (ListBuffer is mutable, List isn't)
-
+/* OLD SHORTEST PATH
+// Shortest Path
 val sourceId: VertexId = 1 // The source
 val targetId: VertexId = 4 // The target
-val t: VertexId = 5 // Random Id, just to send first message
-val test = List[VertexId](t)
+val test: VertexId = 5 // default vertex id, probably need to change
 // Initialize the graph such that all vertices except the root have distance infinity.
 // Note that now vertices will be of type [VertexId, (distance,id)]
-val initialGraph = graph.mapVertices( (id, _) => if (id == sourceId) (0.0, List(id))
-                      else (Double.PositiveInfinity, List(id)))
+val initialGraph = graph.mapVertices( (id, _) => if (id == sourceId) (0.0, id) else (Double.PositiveInfinity, id))
+
+
 val sssp = initialGraph.pregel((Double.PositiveInfinity, test))(
   (id, dist, newDist) => {
     if (dist._1 < newDist._1) dist // dist(0) = first entry of (distance,id)
@@ -117,7 +125,7 @@ val sssp = initialGraph.pregel((Double.PositiveInfinity, test))(
     // So if distance of source + weight of edge connecting to destination is
     // less than attribute of destination => update
     if (triplet.srcAttr._1 + triplet.attr < triplet.dstAttr._1) {
-      Iterator((triplet.dstId, (triplet.srcAttr._1 + triplet.attr,  triplet.srcAttr._2 ::: List(triplet.dstId))))
+      Iterator((triplet.dstId, (triplet.srcAttr._1 + triplet.attr, triplet.srcId)))
       // Now you change the new vertex to be [id of destination, (distance, sourceId)
     }
     else {
@@ -129,7 +137,7 @@ val sssp = initialGraph.pregel((Double.PositiveInfinity, test))(
     else b
   }
 )
-
-println("Shortest path is: ")
-sssp.vertices.filter(x => x._1 == targetId).map(x => x._2._2).collect()
 */
+
+
+
