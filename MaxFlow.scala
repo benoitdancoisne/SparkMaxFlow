6c354ca579974b7ba588283725eaf94e990de9ab
Emilien Dupont <EmilienDupont@DN0a22b157.SUNet>
2015-05-13 14:28:55 -0700

    Put shortest path in function

--- MaxFlow.scala
+++ MaxFlow.scala
@@ -15,6 +15,7 @@ val vertices = sc.textFile("data/toy-vertices.txt").
 val edges = sc.textFile("data/toy-edges.txt").
                 map(line => line.split(" ")).
                 map(e => Edge(e(0).toLong, e(1).toLong, e(2).toInt))
+                //map(e => Edge(e(0).toLong, e(1).toLong, 0))
 
 // Build RDD of flows (Needs to be var as it is going to be updated)
 var flows: RDD[((VertexId,VertexId),Int)] = edges.map(e => ( (e.srcId,e.dstId), 0) )
@@ -23,89 +24,105 @@ var flows: RDD[((VertexId,VertexId),Int)] = edges.map(e => ( (e.srcId,e.dstId), 
 var residual: Graph[None.type,Int] = Graph(vertices, edges)
 
 
+
 // FOR LOOP, WE WANT TO CLEAN THIS SHIZ UP BAD
 // Which upper bound should we put? How many iterations in for loop?
 // Note that break in scala breaks out of ALL for loops, not just one
 
 // Shortest Path, TODO MAKE THIS INTO A METHOD
+
+
 val sourceId: VertexId = 1 // The source
 val targetId: VertexId = 4 // The target
-val test: VertexId = 5 // default vertex id, probably need to change
 
-// Initialize the graph such that all vertices except the root have distance infinity.
-// Note that now vertices will be of type [VertexId, (distance,mincap,id)]
-// Note that the equivalent of Double.PositiveInfinity  is Int.MaxValue. But we check
-// srcAttr._1 + 1, so we need to make sure there is no overflow, so set it to Int.MaxValue - 1
-// Otherwise Double.PositiveInfinity.toInt?
+def shortestPath ( sourceId: VertexId, targetId: VertexId, graph: Graph[None.type,Int] ) : (Set[(VertexId, VertexId)], Int) = {
+  val test: VertexId = 5 // default vertex id, probably need to change
 
-val initialGraph = residual.mapVertices((id, _) => if (id == sourceId) (0, Int.MaxValue - 1, id)
-else (Int.MaxValue - 1, Int.MaxValue - 1, id))
+  // Initialize the graph such that all vertices except the root have distance infinity.
+  // Note that now vertices will be of type [VertexId, (distance,mincap,id)]
+  // Note that the equivalent of Double.PositiveInfinity  is Int.MaxValue. But we check
+  // srcAttr._1 + 1, so we need to make sure there is no overflow, so set it to Int.MaxValue - 1
+  // Otherwise Double.PositiveInfinity.toInt?
+
+  val initialGraph = graph.mapVertices((id, _) => if (id == sourceId) (0, Int.MaxValue - 1, id)
+  else (Int.MaxValue - 1, Int.MaxValue - 1, id))
 
 
-val sssp = initialGraph.pregel((Int.MaxValue - 1, Int.MaxValue - 1, test))(
+  val sssp = initialGraph.pregel((Int.MaxValue - 1, Int.MaxValue - 1, test))(
 
-  // Vprog
-  (id, dist, newDist) => {
-    if (dist._1 < newDist._1) dist
-    else newDist
-  },
+    // Vprog
+    (id, dist, newDist) => {
+      if (dist._1 < newDist._1) dist
+      else newDist
+    },
 
-  // sendMsg
-  triplet => {
-    // So if distance of source + weight of edge connecting to destination is
-    // less than attribute of destination => update
-    // if capacity along edges == 0; do not propagate message
-    if (triplet.srcAttr._1 + 1 < triplet.dstAttr._1 && triplet.attr > 0) {
-      Iterator((triplet.dstId, (triplet.srcAttr._1 + 1, math.min(triplet.srcAttr._2, triplet.attr), triplet.srcId)))
+    // sendMsg
+    triplet => {
+      // So if distance of source + weight of edge connecting to destination is
+      // less than attribute of destination => update
+      // if capacity along edges == 0; do not propagate message
+      if (triplet.srcAttr._1 + 1 < triplet.dstAttr._1 && triplet.attr > 0) {
+        Iterator((triplet.dstId, (triplet.srcAttr._1 + 1, math.min(triplet.srcAttr._2, triplet.attr), triplet.srcId)))
+      }
+      else {
+        Iterator.empty
+      }
+    },
+
+    // mergeMsg
+    (a, b) => {
+      if (a._1 < b._1) a
+      else if ((a._1 == b._1) && (a._2 > b._2)) a
+      else b
     }
-    else {
-      Iterator.empty
-    }
-  },
+  )
 
-  // mergeMsg
-  (a, b) => {
-    if (a._1 < b._1) a
-    else if ((a._1 == b._1) && (a._2 > b._2)) a
-    else b
+  /* TODO :
+
+      stop pregel sp when target node is hit --> pregel API does not seem to offer the possibility
+      maybe add an extra case in the sendMsg and send an empty iterator if target node is hit?
+  */
+
+  // What happens here if nodes are unvisited? We do not want those in our set. Add if != INF statement?
+  val vNum = sssp.vertices.count.toInt // Number of elements < n
+  val v = sssp.vertices.take(vNum) // Convert RDD to array
+  val minCap = sssp.vertices.filter(v => v._1 == targetId).first._2._2
+  val path = Set[(VertexId, VertexId)]()
+
+
+  // CHECK IF THERE IS A PATH OR NOT, if no path => return empty set
+  if (minCap != (Int.MaxValue - 1) ) {
+    val links = new HashMap[VertexId, VertexId]
+    for (i <- 0 to vNum - 1) {
+      links += v(i)._1 -> v(i)._2._3
+    }
+
+    // Build the set of edges in the shortest path
+    var id = targetId
+    val loop = new Breaks // Needed to do break
+    // Might not need this, can just add it in for loop
+    for (i <- 0 to vNum - 1; if id != sourceId) {
+      //if (id == sourceId) {
+      //  loop.break
+      //}
+      path += ((links(id), id))
+      id = links(id)
+    }
   }
-)
 
-/* TODO :
-
-    stop pregel sp when target node is hit --> pregel API does not seem to offer the possibility
-    maybe add an extra case in the sendMsg and send an empty iterator if target node is hit?
-*/
-
-// What happens here if nodes are unvisited? We do not want those in our set. Add if != INF statement?
-val vNum = sssp.vertices.count.toInt // Number of elements < n
-val v = sssp.vertices.take(vNum) // Convert RDD to array
-val minCap = sssp.vertices.filter(v => v._1 == targetId).first._2._2
-
-val links = new HashMap[VertexId, VertexId]
-for (i <- 0 to vNum - 1) {
-  links += v(i)._1 -> v(i)._2._3
+  return (path, minCap)
 }
 
-// Build the set of edges in the shortest path
-
-val path = Set[(VertexId, VertexId)]()
-var id = targetId
-val loop = new Breaks // Needed to do break
-for (i <- 0 to vNum - 1) {
-  if (id == sourceId) {
-    loop.break
-  }
-  path += ((links(id), id))
-  id = links(id)
-}
+val shortest = shortestPath(sourceId, targetId, residual)
+val path = shortest._1
+val minCap = shortest._2
 
 val bcPath = sc.broadcast(path)
 
 // Update the flows RDD
 // TODO: update flows and not otherflows
 
-val newFlows = flows.map(e => {
+var newFlows = flows.map(e => {
   if (bcPath.value contains e._1) {
     (e._1, e._2 + minCap)
   }
